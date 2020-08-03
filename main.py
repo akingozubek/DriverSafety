ad7c4dd730fd72e3cc3275505acc2a92891ac580
akingozubek <akingozubek@gmail.com>
2020-08-03 17:08:58 +0300

    face-eye detect added

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 import cv2
+import numpy as np
+import os
+
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+eye_cascade=cv2.CascadeClassifier("haarcascade_eye.xml")
+
+
+cam=cv2.VideoCapture(0)
+
+
+while True:
+    ret,img=cam.read()
+    gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces=face_cascade.detectMultiScale(gray,1.5,5)
+    for(x,y,w,h) in faces:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+        roi_gray=gray[y:y+h,x:x+w]
+        roi_img=img[y:y+h,x:x+w]
+        eyes=eye_cascade.detectMultiScale(roi_gray)
+
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_img,(ex,ey),(ex+ew,ey+eh),(255,0,0),2)
+    cv2.imshow("img",img)
+    k=cv2.waitKey(30) & 0x0ff
+    if k==27:
+        break
+
+cam.release()
+cv2.destroyAllWindows()
